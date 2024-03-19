168479f307350523ba53d3d3dc482a363cef424e
syyyl <56541363+syyyl@users.noreply.github.com>
2024-03-19 18:20:16 +0900

    Add files via upload

---
+++ midproj-layout/src/test/java/com/ohgiraffers/midprojlayout/MidprojLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.midprojlayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidprojLayoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
